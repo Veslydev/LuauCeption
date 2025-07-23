d64a0c81c49c5875b22d51a0db24be3db5e88084
jLn0n <45232009+jLn0n@users.noreply.github.com>
2025-07-23 19:46:41 +0800

    feat: made utils/build.sh run the snippet builder
    Signed-off-by: jLn0n <45232009+jLn0n@users.noreply.github.com>

--- utils/build.sh
+++ utils/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+BUILDER_SCRIPT="$(realpath "./utils/builder")"
+MAIN_DIR="$(realpath "./")" # assuming that the script is ran on LuauCeption/ directory
+
 mkdir build
 cd build
 emcmake cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo
 cmake --build . --target Luau.LuauCeption.Compiler Luau.LuauCeption.VM Luau.LuauCeption.Full --config RelWithDebInfo -j 2
+
+cd "$MAIN_DIR" 
+lune run "$BUILDER_SCRIPT"
