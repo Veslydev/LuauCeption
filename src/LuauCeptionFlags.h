af4917052ff2f1962fdd7a64a80a868b82c52dc8
jLn0n <45232009+jLn0n@users.noreply.github.com>
2025-07-25 03:33:50 +0800

    chore: renamed to LC_API
    Signed-off-by: jLn0n <45232009+jLn0n@users.noreply.github.com>

--- src/LuauCeptionFlags.h
+++ src/LuauCeptionFlags.h
@@ -1,8 +1,8 @@
 #include <string>
 
 #include <emscripten.h>
-#define EXPORT extern "C" EMSCRIPTEN_KEEPALIVE
+#define LC_API extern "C" EMSCRIPTEN_KEEPALIVE
 
-EXPORT bool set_flag(std::string name, bool state);
-EXPORT void set_all_flags(bool state);
-EXPORT void reset_flags();
+LC_API bool set_flag(std::string name, bool state);
+LC_API void set_all_flags(bool state);
+LC_API void reset_flags();
