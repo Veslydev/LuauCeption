85b273f62e855f1efce616eae8384c1c75840247
Vesly <74131882+Veslydev@users.noreply.github.com>
2026-02-24 18:13:47 +0300

    awdawdas

--- utils/build.sh
+++ utils/build.sh
@@ -12,8 +12,8 @@ fi
 
 mkdir build
 cd build
-emcmake cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo
-cmake --build . --target Luau.LuauCeption.Compiler Luau.LuauCeption.VM Luau.LuauCeption.Full --config RelWithDebInfo -j 2
+emcmake cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake --build . --target Luau.LuauCeption.Compiler Luau.LuauCeption.VM Luau.LuauCeption.Full --config Release -j 2
 
 cd "$MAIN_DIR" 
 lune run "$BUILDER_SCRIPT" -f BUILD_ONEFILE
